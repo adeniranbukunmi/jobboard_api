19595012eb31f7a79d2e4496a087f07519f0c9ae
Adeniran Bukunmi <oluwabukunmiade1@gmail.com>
2026-05-29 15:12:14 +0100

    fix: remove .env from tracking, add to gitignore

--- userApp/urls.py
+++ userApp/urls.py
@@ -5,7 +5,6 @@ from . import views as vw
 
 
 urlpatterns = [
-    path("list_create/", vw.list_create_user, name="register_user"),
-    path("login/", vw.login_user, name="login"),
-    path("edit_profile/<int:pk>", vw.update_profile, name="edit_profile")
+    path("list_create/", vw.creatListUser, name="list_create"),
+    path("login/", vw.login, name="login"),
 ]
